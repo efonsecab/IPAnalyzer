a3016dd2591a4ae4912e3829212a3770b1be242a
Eduardo Fonseca <eduardo.fonseca@pticostarica.com>
2020-09-01 16:10:38 -0600

    Adding tests for ping

---
+++ IPAnalyzerSln/IPAnalyzer.AutomatedTests/FrameworkBaseAPIs/PingTests.cs
@@ -0,0 +1,28 @@
+﻿using IPAnalyzer.AutomatedTests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IPAnalyzer.AutomatedTests.FrameworkBaseAPIs
+{
+    [TestClass]
+    public class PingTests
+    {
+        [TestMethod]
+        public async Task Test_PingIpAddress()
+        {
+            int timeoutMilliSeconds = 1000;
+            Ping ping = new Ping();
+            var pingReply = await ping.SendPingAsync(Constants.TEST_REVERSEDNS_HOSTNAME, timeout:timeoutMilliSeconds);
+            Assert.IsTrue(pingReply.Status == IPStatus.Success);
+            pingReply = await ping.SendPingAsync(Constants.TEST_HOSTNAME, timeout: timeoutMilliSeconds);
+            Assert.IsTrue(pingReply.Status == IPStatus.TimedOut);
+        }
+    }
+}
--- IPAnalyzerSln/IPAnalyzer.AutomatedTests/Helpers/Constants.cs
+++ IPAnalyzerSln/IPAnalyzer.AutomatedTests/Helpers/Constants.cs
@@ -4,7 +4,7 @@ namespace IPAnalyzer.AutomatedTests.Helpers
     public class Constants
     {
         public const string TEST_HOSTNAME = "microsoft.com";
-        public const string TEST_IP_V4 = "201.203.6.13";
+        public const string TEST_IP_V4 = "23.199.250.4";
         public const string TEST_REVERSEDNS_HOSTNAME = "www.microsoft.com";
     }
 }
